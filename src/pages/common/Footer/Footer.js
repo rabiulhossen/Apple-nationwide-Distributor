93c58c096ae9fedd1cb225b510d00254ea914f0e
rabiulhossen <rhsimon69@gmail.com>
2022-05-07 20:19:40 +0600

    footer completed

--- src/pages/common/Footer/Footer.js
+++ src/pages/common/Footer/Footer.js
@@ -1,13 +1,82 @@
-import React from 'react';
+import React from "react";
+import "./Footer.css";
+import link from "../../../img/linkedin.png";
+import fb from "../../../img/facebook.png";
+import insta from "../../../img/instagram.png";
+import reddit from "../../../img/reddit.png";
 
 const Footer = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    return (
-        <footer className='text-center mt-5'>
-            <p><small>copyright @ {year} </small></p>
-        </footer>
-    );
+  const today = new Date();
+  const year = today.getFullYear();
+  return (
+    <footer className="text-center mt-5 footer-clean">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-sm-4 col-md-3 item">
+            <h3>Services</h3>
+            <ul>
+              <li>
+                <a href="#">Whole seller</a>
+              </li>
+              <li>
+                <a href="#">installment process</a>
+              </li>
+              <li>
+                <a href="#">Cheapest Price</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-sm-4 col-md-3 item">
+            <h3>About</h3>
+            <ul>
+              <li>
+                <a href="#">Company</a>
+              </li>
+              <li>
+                <a href="#">Team</a>
+              </li>
+              <li>
+                <a href="#">Legacy</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-sm-4 col-md-3 item">
+            <h3>Careers</h3>
+            <ul>
+              <li>
+                <a href="#">Job openings</a>
+              </li>
+              <li>
+                <a href="#">Employee success</a>
+              </li>
+              <li>
+                <a href="#">Benefits</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 item social">
+            <a href="#">
+              <img src={link} style={{ height: "45px" }}></img>
+            </a>
+            <a href="#">
+             
+              <img src={fb} style={{ height: "45px" }}></img>
+            </a>
+        
+            <a href="#">
+              <img src={insta} style={{ height: "45px" }}></img>
+            </a>
+            <a href="#">
+              <img src={reddit} style={{ height: "45px" }}></img>
+            </a>
+            <p className="mt-3">
+              copyright @ {year} 
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/common/Header/Header.js
+++ src/pages/common/Header/Header.js
@@ -6,18 +6,18 @@ const Header = () => {
         <div>
             <Navbar bg="info" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand to="/home">React-Bootstrap</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+        <Nav.Link to="/home">Home</Nav.Link>
+        <Nav.Link to="/link">Link</Nav.Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Item to="/action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item to="/action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item to="/action/3.3">Something</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown.Item to="/action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
