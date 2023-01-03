db547ef7b6370d7456ffc7198a1d730dad6ef37d
Rabiul-Hossen <rhsimon69@gmail.com>
2023-01-04 01:37:33 +0600

    login component style changed

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,114 +1,138 @@
-import React, { useRef } from "react";
-import auth from "../../../firebase.init";
-import Spinner from "../../common/Spinner/Spinner";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Form, Toast, ToastContainer } from "react-bootstrap";
+import React, { useRef } from 'react';
+import auth from '../../../firebase.init';
+import Spinner from '../../common/Spinner/Spinner';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
 import './Loggin.css';
 
-import PageTitle from "../../common/PageTitle/PageTitle";
+import PageTitle from '../../common/PageTitle/PageTitle';
 import {
-  useSignInWithEmailAndPassword,
-  useSendPasswordResetEmail,
-} from "react-firebase-hooks/auth";
+   useSignInWithEmailAndPassword,
+   useSendPasswordResetEmail,
+} from 'react-firebase-hooks/auth';
 
-import Google from "./Google/Google";
-import { Alert } from "bootstrap";
-import Popup from "../Popup/Popup";
+import Google from './Google/Google';
 
 const Login = () => {
-  const emailRef = useRef('');
-  const passwordRef = useRef('');
-  const navigate = useNavigate();
-  const location = useLocation();
+   const emailRef = useRef('');
+   const passwordRef = useRef('');
+   const navigate = useNavigate();
+   const location = useLocation();
 
-  let from = location.state?.from?.pathname || "/";
-  let errorElement;
-  const [SignInWithEmailAndPassword, user, loading, error] =
-    useSignInWithEmailAndPassword(auth);
-  const [SendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+   let from = location.state?.from?.pathname || '/';
+   let errorElement;
+   const [SignInWithEmailAndPassword, user, loading, error] =
+      useSignInWithEmailAndPassword(auth);
+   const [SendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
-  if (loading || sending) {
-    return <Spinner></Spinner>;
-  }
-  if (user) {
-    navigate("/inventory");
-  }
-  if (error) {
-    errorElement = <p className="text-danger"> Error: {error?.message}</p>;
-  }
+   if (loading || sending) {
+      return <Spinner></Spinner>;
+   }
+   if (user) {
+      navigate('/inventory');
+   }
+   if (error) {
+      errorElement = <p className="text-danger"> Error: {error?.message}</p>;
+   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
 
-    SignInWithEmailAndPassword(email, password);
-  };
-  const navigateRegister = (event) => {
-    navigate("/register");
-  };
-  const passwordReset = async () => {
-    const email = emailRef.current.value;
-    if (email) {
-      await SendPasswordResetEmail(email);
-     alert('check your email')
-  
-    } else {
-      alert("enter your email address");
-    }
-  };
+      SignInWithEmailAndPassword(email, password);
+   };
+   const navigateRegister = (event) => {
+      navigate('/register');
+   };
+   const passwordReset = async () => {
+      const email = emailRef.current.value;
+      if (email) {
+         await SendPasswordResetEmail(email);
+         alert('check your email');
+      } else {
+         alert('enter your email address');
+      }
+   };
 
-  return (
-    <div className="container w-50 mx-auto mt-5 pt-5">
-      <PageTitle title="Login"></PageTitle>
-      <h1 className="text-primary text-center mt-2">Please Login</h1>
+   return (
+      <div className="container w-25 mx-auto mt-5 pt-5 login-card">
+         <div className="form-container">
+            <PageTitle title="Login"></PageTitle>
+            <div className="form">
+               <h1 className="text-primary text-center mt-2">Please Login</h1>
 
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="my-3" controlId="formBasicEmail">
-          <Form.Control
-            ref={emailRef}
-            type="email"
-            placeholder="Enter email"
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control
-            ref={passwordRef}
-            type="password"
-            placeholder="Password"
-            required
-          />
-        </Form.Group>
-        <button className="button-submit px-5 py-3" type="submit">
-          Login
-        </button>
-      </Form>
-      {errorElement}
-      <p className="pt-3">
-        New to <span className="fw-bold">Apple Nationwide</span>?
-        <Link
-          to="/register"
-          className="text-primary pe-auto text-decoration-none"
-          onClick={navigateRegister}
-        >
-          Please Register
-        </Link>
-      </p>
-      <p>
-        Forget Password?
-        <button
-          onClick={passwordReset}
-          className="btn btn-link text-primary pe-auto text-decoration-none"
-        >
-          Reset Password
-        </button>
-      </p>
+               <Form onSubmit={handleSubmit}>
+                  
+                     <Form.Group className="form-control" controlId="formBasicEmail">
+                        <Form.Control
+                           ref={emailRef}
+                           type="email"
+                          
+                           required
+                           className='input'
+                        /> <label>
+                        <span style={{transitionDelay:"0ms"}}>U</span>
+                        <span style={{transitionDelay:"50ms"}}>s</span>
+                        <span style={{transitionDelay:"100ms"}}>e</span>
+                        <span style={{transitionDelay:"150ms"}}>r</span>
+                        <span style={{transitionDelay:"200ms"}}>E</span>
+                        <span style={{transitionDelay:"250ms"}}>m</span>
+                        <span style={{transitionDelay:"300ms"}}>a</span>
+                        <span style={{transitionDelay:"350ms"}}>i</span>
+                        <span style={{transitionDelay:"400ms"}}>l</span>
+                     </label>
+                     </Form.Group>
+                    
+                  
+                  
+                     <Form.Group className=" form-control" controlId="formBasicPassword">
+                        <Form.Control
+                           ref={passwordRef}
+                           type="password"
+                        
+                           required
+                           className='input'
+                        />
+                        <label>
+                        <span style={{transitionDelay:"0ms"}}>U</span>
+                        <span style={{transitionDelay:"50ms"}}>s</span>
+                        <span style={{transitionDelay:"100ms"}}>e</span>
+                        <span style={{transitionDelay:"150ms"}}>r</span>
+                     </label>
+                     </Form.Group>
+                     
+                
+                  <button className="button px-5 py-3" type="submit">
+                     Login
+                  </button>
+               </Form>
+               {errorElement}
+               <p className="pt-3">
+                  New to <span className="fw-bold">Apple Nationwide</span>?
+                  <Link
+                     to="/register"
+                     className="text-primary pe-auto text-decoration-none"
+                     onClick={navigateRegister}
+                  >
+                     Please Register
+                  </Link>
+               </p>
+               <p>
+                  Forget Password?
+                  <button
+                     onClick={passwordReset}
+                     className="btn btn-link text-primary pe-auto text-decoration-none"
+                  >
+                     Reset Password
+                  </button>
+               </p>
 
-      <Google></Google>
-    </div>
-  );
+               <Google></Google>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Login;
-
--- src/pages/common/Header/Header.js
+++ src/pages/common/Header/Header.js
@@ -3,9 +3,14 @@ import { signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import auth from "../../../firebase.init";
-import theme from "../../../img/Theme.jpg";
-import { Button } from "bootstrap";
-import { Nav } from "react-bootstrap";
+import theme from "../../../img/theme.gif";
+
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const Header = () => {
   const [user] = useAuthState(auth);
@@ -13,83 +18,117 @@ const Header = () => {
     signOut(auth);
   };
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary fw-bold fs-4 sticky-top mb-1">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/home">
-          <img style={{ height: "50px" }} src={theme} className="rounded"></img>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/home">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" to="/inventory">
-                Inventory
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link active" to="/blog">
-                Blog
-              </Link>
-            </li>
-            {user ? (
-              <>
-                <li className="nav-item">
-                  <Link
-                    to="/"
-                className="text-dark px-4 nav-link active"
-                    onClick={handleSignOut}
-                  >
-                    Sign Out
-                  </Link>{" "}
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link active text-dark" to="/myitem">
-                    My Items
-                  </Link>{" "}
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link active text-dark" to="/addnewitem">
-                    Add Item
-                  </Link>{" "}
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link active text-dark" to="/maintenance">
-                    Manage Item
-                  </Link>{" "}
-                </li>
-              </>
-            ) : (
-              <li className="nav-item">
-                <Link className="nav-link active text-dark" to="/login">
-                  Login
-                </Link>
-              </li> 
-            )}  
-          </ul>
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-info fw-medium fs-4 sticky-top mb-1">
+    //   <div className="container-fluid">
+    //   
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-bs-toggle="collapse"
+    //       data-bs-target="#navbarNav"
+    //       aria-controls="navbarNav"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarNav">
+    //       <ul className="navbar-nav">
+    //         <li className="nav-item">
+    //           <Link className="nav-link active" aria-current="page" to="/home">
+    //             Home
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link active" to="/inventory">
+    //             Inventory
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link active" to="/about">
+    //             About
+    //           </Link>
+    //         </li>
+    //         <li className="nav-item">
+    //           <Link className="nav-link active" to="/blog">
+    //             Blog
+    //           </Link>
+    //         </li>
+    //         {user ? (
+    //           <>
+    //             <li className="nav-item">
+    //               <Link
+    //                 to="/"
+    //             className="text-dark px-4 nav-link active"
+    //                 onClick={handleSignOut}
+    //               >
+    //                 Sign Out
+    //               </Link>{" "}
+    //             </li>
+    //             <li className="nav-item">
+    //               <Link className="nav-link active text-dark" to="/myitem">
+    //                 My Items
+    //               </Link>{" "}
+    //             </li>
+    //             <li className="nav-item">
+    //               <Link className="nav-link active text-dark" to="/addnewitem">
+    //                 Add Item
+    //               </Link>{" "}
+    //             </li>
+    //             <li className="nav-item">
+    //               <Link className="nav-link active text-dark" to="/maintenance">
+    //                 Manage Item
+    //               </Link>{" "}
+    //             </li>
+    //           </>
+    //         ) : (
+    //           <li className="nav-item">
+    //             <Link className="nav-link active text-dark" to="/login">
+    //               Login
+    //             </Link>
+    //           </li> 
+    //         )}  
+    //       </ul>
         
-        </div>
-      </div>
-    </nav>
+    //     </div>
+    //   </div>
+    // </nav>
+
+
+<Navbar collapseOnSelect expand="lg" bg="info" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"><img style={{ height: "45px",width:"80px" }} src={theme} className="rounded"></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {/* <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav> */}
+          <Nav className="d-flex justify-content-end ">
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+   
+ 
+
+
   );
 };
 
