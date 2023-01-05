3907e0c0e224dbff5131a54dd8b63a6bd44851a3
Rabiul-Hossen <rhsimon69@gmail.com>
2023-01-06 00:23:06 +0600

    componet editing

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import auth from '../../../firebase.init';
 import Spinner from '../../common/Spinner/Spinner';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
 import './Loggin.css';
+import Notifications, { notify } from 'react-notify-toast';
 import PageTitle from '../../common/PageTitle/PageTitle';
 import {
    useSignInWithEmailAndPassword,
@@ -17,114 +18,157 @@ const Login = () => {
    const passwordRef = useRef('');
    const navigate = useNavigate();
    const location = useLocation();
+   const [email, setEmail] = useState('');
 
    let from = location.state?.from?.pathname || '/';
+
    let errorElement;
+
    const [SignInWithEmailAndPassword, user, loading, error] =
       useSignInWithEmailAndPassword(auth);
-   const [SendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
-   if (loading || sending) {
+   if (loading) {
       return <Spinner></Spinner>;
    }
    if (user) {
-      navigate('/inventory');
+      navigate('/home');
    }
    if (error) {
-      errorElement = <p className="text-danger"> Error: {error?.message}</p>;
+      errorElement = (
+         <code className="text-danger">Error:Incorrect email or password </code>
+      );
    }
+
+   const SendPasswordReset = () => {
+      
+      const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(
+        auth
+      );
+    
+      if (error) {
+        return (
+          <div>
+            <p>Error: {error.message}</p>
+          </div>
+        );
+      }
+      if (sending) {;
+        return <p>Sending...</p>;
+      }
+      if(email){
+         sendPasswordResetEmail(email)
+      }}
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      const email = emailRef.current.value;
+      const email =emailRef.current.value;
       const password = passwordRef.current.value;
-
+      console.log(email, password);
       SignInWithEmailAndPassword(email, password);
    };
    const navigateRegister = (event) => {
       navigate('/register');
    };
-   const passwordReset = async () => {
-      const email = emailRef.current.value;
-      if (email) {
-         await SendPasswordResetEmail(email);
-         alert('check your email');
-      } else {
-         alert('enter your email address');
-      }
-   };
+
+   let myColor = { background: '#0E1717', text: '#FFFFFF' };
+
+   // const passwordReset = async () => {
+   //    const email = emailRef.current.value;
+   //    if (email) {
+   //       await SendPasswordResetEmail(email);
+
+   //       notify.show("this is sample text", "succes", 5000, myColor);
+   //    } else {
+
+   //       notify.show("this is sample text", "warning", 5000, myColor);
+   //    }
+   // };
 
    return (
-      <div className="container w-25 mx-auto mt-5 pt-3 pb-4 login-card">
+      <div
+         className="container mx-auto mt-5 pt-3 pb-4 login-card"
+         style={{ width: '26rem' }}
+      >
+         <Notifications />
          <div className="form-container">
             <PageTitle title="Login"></PageTitle>
             <div className="form">
-               <h1 className="text-primary text-center my-4">Please Login</h1>
+               <h2 className="text-primary text-center mb-5 mt-3">
+                  Access Your Account!
+               </h2>
 
                <Form onSubmit={handleSubmit}>
-                  
-                     <Form.Group className="form-control" controlId="formBasicEmail">
-                        <Form.Control
-                           ref={emailRef}
-                           type="email"
-                          
-                           required
-                           className='input'
-                        /> <label>
-                        <span style={{transitionDelay:"0ms"}}>U</span>
-                        <span style={{transitionDelay:"50ms"}}>s</span>
-                        <span style={{transitionDelay:"100ms"}}>e</span>
-                        <span style={{transitionDelay:"150ms"}}>r</span>
-                        <span style={{transitionDelay:"200ms"}}>E</span>
-                        <span style={{transitionDelay:"250ms"}}>m</span>
-                        <span style={{transitionDelay:"300ms"}}>a</span>
-                        <span style={{transitionDelay:"350ms"}}>i</span>
-                        <span style={{transitionDelay:"400ms"}}>l</span>
+                  <Form.Group
+                     className="form-control"
+                     controlId="formBasicEmail"
+                  >
+                     <Form.Control
+                        type="email"
+                       ref={emailRef}
+                        required
+                        className="input"
+                     />{' '}
+                     <label>
+                        <span style={{ transitionDelay: '0ms' }}>U</span>
+                        <span style={{ transitionDelay: '50ms' }}>s</span>
+                        <span style={{ transitionDelay: '100ms' }}>e</span>
+                        <span style={{ transitionDelay: '150ms' }}>r</span>
+                        <span style={{ transitionDelay: '200ms' }}>E</span>
+                        <span style={{ transitionDelay: '250ms' }}>m</span>
+                        <span style={{ transitionDelay: '300ms' }}>a</span>
+                        <span style={{ transitionDelay: '350ms' }}>i</span>
+                        <span style={{ transitionDelay: '400ms' }}>l</span>
                      </label>
-                     </Form.Group>
-                    
-                  
-                  
-                     <Form.Group className=" form-control" controlId="formBasicPassword">
-                        <Form.Control
-                           ref={passwordRef}
-                           type="password"
-                           required
-                           className='input'
-                        />
-                        <label>
-                        <span style={{transitionDelay:"0ms"}}>P</span>
-                        <span style={{transitionDelay:"50ms"}}>a</span>
-                        <span style={{transitionDelay:"100ms"}}>s</span>
-                        <span style={{transitionDelay:"150ms"}}>s</span>
-                        <span style={{transitionDelay:"200ms"}}>w</span>
-                        <span style={{transitionDelay:"250ms"}}>o</span>
-                        <span style={{transitionDelay:"300ms"}}>r</span>
-                        <span style={{transitionDelay:"350ms"}}>d</span>
+                  </Form.Group>
+
+                  <Form.Group
+                     className=" form-control"
+                     controlId="formBasicPassword"
+                  >
+                     <Form.Control
+                        ref={passwordRef}
+                        type="password"
+                        required
+                        className="input"
+                     />
+                     <label>
+                        <span style={{ transitionDelay: '0ms' }}>P</span>
+                        <span style={{ transitionDelay: '50ms' }}>a</span>
+                        <span style={{ transitionDelay: '100ms' }}>s</span>
+                        <span style={{ transitionDelay: '150ms' }}>s</span>
+                        <span style={{ transitionDelay: '200ms' }}>w</span>
+                        <span style={{ transitionDelay: '250ms' }}>o</span>
+                        <span style={{ transitionDelay: '300ms' }}>r</span>
+                        <span style={{ transitionDelay: '350ms' }}>d</span>
                      </label>
-                     </Form.Group>
-                     
-                
-                  <button className="button px-5 py-2 mb-2" type="submit">
-                     Login
+                  </Form.Group>
+
+                  <button className="button px-4 py-2 mb-2" type="submit">
+                     Sign In
                   </button>
                </Form>
                {errorElement}
-               <p className="pt-2">
-                  New to <span className="fw-bold ps-1"> Apple Nationwide</span>?
+               <p className="pt-3 pb-0 mb-1">
+                  New to{' '}
+                  <span className="fw-semiBold ps-1"> Apple Nationwide</span>?
                   <Link
                      to="/register"
                      className="text-primary ps-1 text-decoration-none"
                      onClick={navigateRegister}
                   >
-                      Please Register
+                     Please Register
                   </Link>
                </p>
                <p>
                   Forget Password?
                   <button
-                     onClick={passwordReset}
-                     className="btn btn-link text-primary pe-1 text-decoration-none"
+                     onClick={async () => {
+                        const success = await SendPasswordReset(email);
+                        if (success) {
+                           alert('Sent email');
+                        }
+                     }}
+                     className="btn btn-link text-primary ps-1 pt-0 text-decoration-none"
                   >
                      Reset Password
                   </button>
--- src/pages/Login/Register/Register.js
+++ src/pages/Login/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Form } from 'react-bootstrap';
 import {
    useCreateUserWithEmailAndPassword,
@@ -17,6 +17,9 @@ const Register = () => {
       useCreateUserWithEmailAndPassword(auth);
 
    const [setProfile, updating, setError] = useUpdateProfile(auth);
+   const nameRef = useRef('');
+   const emailRef = useRef('');
+   const passwordRef = useRef('');
    const navigate = useNavigate();
    const navigation = () => {
       navigate('/inventory');
@@ -34,70 +37,72 @@ const Register = () => {
 
    const handleRegister = async (event) => {
       event.preventDefault();
-      const name = event.target.name.value;
-      const email = event.target.email.value;
-      const password = event.target.password.value;
-
+      const name = nameRef.current.value;
+      const email =emailRef.current.value;
+      const password = passwordRef.current.value;
+      console.log(name,email,password);
       await createUserWithEmailAndPassword(email, password);
       await setProfile({ displayName: name });
       console.log('Updated profile');
       navigate('/inventory');
    };
    return (
-      <div className="text-center container w-25 mx-auto mt-5 pt-3 pb-4 register-div">
-         <h1 className="mb-3">Please Register</h1>
+      <div className="text-center container mx-auto mt-5 pt-3 pb-4 register-div"  style={{width: "30rem"}}>
+         <h1 className="mb-3">Create an Account</h1>
          <form onSubmit={handleRegister} className="form-group">
             <div class="form mx-auto">
                <input
                   class="input"
-                  placeholder="Type your text"
-                  required=""
+                  placeholder="Type your Name"
+               
+                ref={nameRef}
                   type="text"
+                  required
                />
                <span class="input-border"></span>
             </div>
             <div class="form  mx-auto">
                <input
                   class="input"
-                  placeholder="Type your text"
-                  required=""
-                  type="text"
+                  placeholder="Type your Email"
+                
+                  type="email"
+                  ref={emailRef}
+                  required
                />
                <span class="input-border"></span>
             </div>
             <div class="form mx-auto">
                <input
                   class="input"
-                  placeholder="Type your text"
-                  required=""
-                  type="text"
+                  placeholder="Type your Password"
+                  minLength={6}
+               
+                  ref={passwordRef}
+                  type="password"
+                  required
                />
                <span class="input-border"></span>
             </div>
 
-            {/* <input
+            <input
                onClick={() => setSignUp(!signUp)}
                type="checkbox"
                name="terms"
                id="terms"
             />
             <label
-               className={`ps-2 ${signUp ? '' : 'text-danger'}`}
+               className={`ps-2 pt-2 ${signUp ? '' : 'text-danger'}`}
                htmlFor="terms"
             >
-               Accept Apple Disttributor Terms and Conditions
+               Accept Apple's Terms and Conditions
             </label>
-            <br /> */}
-            {/* <input
-               disabled={!signUp}
-               className="register-btn"
-               type="submit"
-               value="Register"
-            /> */}
+          
+           
 
-<button className='register-btn mx-auto'>
+<button className='register-btn mx-auto' disabled={!signUp}>
     <span>Continue</span>
-    <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg  width="30" height="30" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
         <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
     </svg>
