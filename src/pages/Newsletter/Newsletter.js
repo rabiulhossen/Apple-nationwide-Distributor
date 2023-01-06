2a88a7d9f3e87ff4baa1f8ee75a0f19c1beff16e
Rabiul-Hossen <rhsimon69@gmail.com>
2023-01-07 00:14:39 +0600

    newsletter integrated with emailjs

--- src/pages/Newsletter/Newsletter.js
+++ src/pages/Newsletter/Newsletter.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Newsletter.css';
+import emailjs from '@emailjs/browser';
 
 export default function Newsletter() {
+
+const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+     emailjs.sendForm('apple-world', 'template_fcgfy2c', form.current, '1NZeW9SAwUpK04-h_')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
    return (
       <div className=' container mt-5 mx-auto'>
 <h2 className='fw-bold mb-3'>Stay Tuned with Us</h2>
          <div class="input-group">
+            <form action=""  onSubmit={sendEmail}   ref={form}  >
             <input
+        
                type="email"
                class="input-newsletter"
                id="Email"
                name="Email"
                placeholder="Enter your email"
                autocomplete="off"
+               required
             />
             <input class="button--submit" value="Subscribe" type="submit" />
+            </form>
          </div>
       </div>
    );
